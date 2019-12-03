da80697f1ea1abc71eb4c3f13591bb2b24f3eefb
fermersky <forte201253@gmail.com>
2019-12-03 11:23:32 +0200

    HintSmall component

--- react-client/src/components/HintSmall/HintSmall.js
+++ react-client/src/components/HintSmall/HintSmall.js
@@ -3,13 +3,26 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import './HintSmall.css';
+import TagsList from '../TagsList/TagsList';
+import HintAuthorContainer from '../../containers/HintAuthorContainer';
 
 const HintsList = ({ hint }) => {
     return (
-        <div className="hint-sm-container">
-            <Link to={'/hint/' + hint.slug}>
-                <h2>{hint.title}</h2>
+        <div className="hint-sm-container animated fadeInUp">
+            <h3>
+                <HintAuthorContainer type="min" userId={hint.user_id} />
+            </h3>
+
+            <Link className="hint-link" to={'/hint/' + hint.slug}>
+                <h2 className="hint-link--content">
+                    {hint.title}
+                    <div className="hint-link--bg"></div>
+                </h2>
             </Link>
+
+            <div className="tags">
+                <TagsList tags={hint.tags} />
+            </div>
         </div>
     );
 };
